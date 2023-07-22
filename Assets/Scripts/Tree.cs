2a86ae1743bba90f9c300ebf68930cfb866ff1ae
JuliusRumanus <julian.losiscky@davinci.edu.ar>
2023-07-22 18:55:21 -0300

    Script incompleto de arbol agregado

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public GameObject fullTree;
+    public GameObject chopTree;
+    public bool choped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fullTree.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (choped)
+        {
+            fullTree.SetActive(false);
+            chopTree.SetActive(true);
+        }
+    }
+}
