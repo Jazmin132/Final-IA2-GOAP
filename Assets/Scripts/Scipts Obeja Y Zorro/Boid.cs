1ca1127fb750d26c29b7bb2ca78414241d18291e
Jazmin132 <jazi2000.ja@gmail.com>
2023-08-03 22:59:04 -0300

    Revert "Particula sudor"
    This reverts commit 8fa947dc8548b1507b6712dd47d3b7d1b4b021d8.

--- Assets/Scripts/Scipts Obeja Y Zorro/Boid.cs
+++ Assets/Scripts/Scipts Obeja Y Zorro/Boid.cs
@@ -19,7 +19,6 @@ public class Boid : GridEntity
     public float arriveRadius;
     public float eatRadius;
     public ParticleSystem particleHungry;
-    public ParticleSystem particleHelp;
 
     [Range(0f, 3f)]
     public float separationWeight;
@@ -67,15 +66,13 @@ public class Boid : GridEntity
             else if ((GameManager.instance.food.transform.position - transform.position).magnitude <= arriveRadius)
                 SentToFSM(BoidStates.ARRIVE);
         };
-
-        _Evade.OnEnter += x => { particleHelp.Play(); };
+       
         _Evade.OnFixedUpdate += () => 
         {
             AddForce(Evade() * evadeWeight);
             if (Vector3.Distance(transform.position, hunter.transform.position) > viewRadius)
                 SentToFSM(BoidStates.ALIGNMENT);
         };
-        _Evade.OnExit += x => { particleHelp.Stop(); };
 
         _Arrive.OnEnter += x => { particleHungry.Play(); };
         _Arrive.OnFixedUpdate += () =>
