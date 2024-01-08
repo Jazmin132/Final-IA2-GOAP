f5409dc9f007309b89dd4c487283206e5f9edc32
REYGON2468 <gonzalo.longatti@davinci.edu.ar>
2024-01-08 18:35:50 -0300

    CanteenScriptsStarting

--- Assets/Scripts/Extras/Canteen.cs
+++ Assets/Scripts/Extras/Canteen.cs
@@ -7,6 +7,38 @@ public class Canteen : MonoBehaviour
     public float foodQuantity;
     public GameObject food;
 
+    public List<Apple> appleQuantity = new List<Apple>();
+    public List<Coconut> coconutQuantity = new List<Coconut>();
+    public List<Bean> beanQuantity = new List<Bean>();
+
+    public int maxQuantityOfFood, maxTypesOfFood, randomNumFood;
+
+    void Start()
+    {
+        for (int i = 0; i < maxQuantityOfFood; i++)
+        {
+            randomNumFood = Random.Range(0, maxTypesOfFood);
+
+            switch (randomNumFood)
+            {
+                case 0:
+
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                default:
+
+                    Debug.Log("ERROR, no se encuentra una comida de el número" + randomNumFood);
+
+                    break;
+            }
+        }
+    }
+
     public void AddFood(float quantity)
     {
         if (foodQuantity > 0)
@@ -24,4 +56,6 @@ public class Canteen : MonoBehaviour
         }
         foodQuantity -= quantity;
     }
+
+
 }
