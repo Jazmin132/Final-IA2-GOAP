d66f09da49f8b0cfbdcf9b3e02f16d9e637af583
REYGON2468 <gonzalo.longatti@davinci.edu.ar>
2024-01-08 19:11:46 -0300

    CanteenScripts+

--- Assets/Scripts/Extras/Canteen.cs
+++ Assets/Scripts/Extras/Canteen.cs
@@ -7,36 +7,17 @@ public class Canteen : MonoBehaviour
     public float foodQuantity;
     public GameObject food;
 
-    public List<Apple> appleQuantity = new List<Apple>();
-    public List<Coconut> coconutQuantity = new List<Coconut>();
-    public List<Bean> beanQuantity = new List<Bean>();
+    public List<Apple> appleListQuantity = new List<Apple>();
+    public List<Coconut> coconutListQuantity = new List<Coconut>();
+    public List<Bean> beanListQuantity = new List<Bean>();
 
-    public int maxQuantityOfFood, maxTypesOfFood, randomNumFood;
+    public int maxQuantityOfFood, maxTypesOfFood;
+
+    int _randomNumFood;
 
     void Start()
     {
-        for (int i = 0; i < maxQuantityOfFood; i++)
-        {
-            randomNumFood = Random.Range(0, maxTypesOfFood);
-
-            switch (randomNumFood)
-            {
-                case 0:
-
-                    break;
-                case 1:
-
-                    break;
-                case 2:
-
-                    break;
-                default:
-
-                    Debug.Log("ERROR, no se encuentra una comida de el número" + randomNumFood);
-
-                    break;
-            }
-        }
+        FoodIngredients();
     }
 
     public void AddFood(float quantity)
@@ -57,5 +38,32 @@ public class Canteen : MonoBehaviour
         foodQuantity -= quantity;
     }
 
+    public void FoodIngredients()
+    {
+        for (int i = 0; i < maxQuantityOfFood; i++)
+        {
+            _randomNumFood = Random.Range(0, maxTypesOfFood);
 
+            switch (_randomNumFood)
+            {
+                case 0:
+                    Debug.Log("appleListQuantity Capacity+");
+                    appleListQuantity.Add(null);
+                    break;
+                case 1:
+                    Debug.Log("coconutListQuantity Capacity+");
+                    coconutListQuantity.Add(null);
+                    break;
+                case 2:
+                    Debug.Log("beanListQuantity Capacity+");
+                    beanListQuantity.Add(null);
+                    break;
+                default:
+                    Debug.Log("ERROR, no se encuentra una comida de el número " + _randomNumFood);
+                    break;
+            }
+
+            Debug.Log(i);
+        }
+    }
 }
