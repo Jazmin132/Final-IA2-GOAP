76670c20c7b37a7f599032fbaf569a0aefb36195
REYGON2468 <gonzalo.longatti@davinci.edu.ar>
2024-01-10 18:12:52 -0300

    SpatialGridCheckIfExisting

--- Assets/TimeSlicing/SpatialGrid.cs
+++ Assets/TimeSlicing/SpatialGrid.cs
@@ -183,8 +183,11 @@ public class SpatialGrid : MonoBehaviour
             {
                 foreach(var neighbour in allElems.Where(x => x != ent))
                 {
-                    Gizmos.DrawLine(ent.transform.position, neighbour.transform.position);//Error tira aca -> Debería de chequear si 'ent' sigue existiendo
-                    connections++;
+                    if (ent != null && neighbour != null)
+                    {
+                        Gizmos.DrawLine(ent.transform.position, neighbour.transform.position);
+                        connections++;
+                    }
                 }
                 if(showLogs)
                     Debug.Log("tengo " + connections + " conexiones por individuo");
