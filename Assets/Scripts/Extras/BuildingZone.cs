1c1058d2f912b12ae5a345a2aacccb7c2e4d87cb
REYGON2468 <gonzalo.longatti@davinci.edu.ar>
2024-02-08 18:31:49 -0300

    BuildingZoneNew

--- Assets/Scripts/Extras/BuildingZone.cs
+++ Assets/Scripts/Extras/BuildingZone.cs
@@ -6,26 +6,69 @@ using UnityEngine.UI;
 public class BuildingZone : MonoBehaviour
 {
     public float materialsQuantity;
-    public GameObject columns1;
-    public GameObject columns2;
-    public GameObject roof;
+    //public GameObject columns1;
+    //public GameObject columns2;
+    //public GameObject roof;
+
+    [SerializeField] GameObject[] _columns;
+    [SerializeField] GameObject[] _walls;
+    [SerializeField] GameObject _entrance;
+    [SerializeField] GameObject _roof;
 
     [SerializeField] Text _materialsText;
 
     public void AddMaterials(float quantity)
     {
         materialsQuantity += quantity;
-        if (materialsQuantity > 250)
+
+        //if (materialsQuantity > 250)
+        //{
+        //    columns1.SetActive(true);
+        //}
+        //else if (materialsQuantity > 500)
+        //{
+        //    columns2.SetActive(true);
+        //}
+        //else if (materialsQuantity > 1000)
+        //{
+        //    roof.SetActive(true);
+        //}
+
+        if(materialsQuantity >= 50)
         {
-            columns1.SetActive(true);
+            _columns[0].SetActive(true);
         }
-        else if (materialsQuantity > 500)
+        else if (materialsQuantity >= 100)
         {
-            columns2.SetActive(true);
+            _walls[0].SetActive(true);
         }
-        else if (materialsQuantity > 1000)
+        else if (materialsQuantity >= 150)
         {
-            roof.SetActive(true);
+            _columns[1].SetActive(true);
+        }
+        else if (materialsQuantity >= 200)
+        {
+            _walls[1].SetActive(true);
+        }
+        else if (materialsQuantity >= 250)
+        {
+            _columns[2].SetActive(true);
+        }
+        else if (materialsQuantity >= 300)
+        {
+            _walls[2].SetActive(true);
+        }
+        else if (materialsQuantity >= 350)
+        {
+            _columns[3].SetActive(true);
+        }
+        else if (materialsQuantity >= 400)
+        {
+            _entrance.SetActive(true);
+        }
+        else if (materialsQuantity >= 500)
+        {
+            _roof.SetActive(true);
         }
 
         _materialsText.text = materialsQuantity.ToString();
