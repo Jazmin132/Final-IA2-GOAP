ac026f13be1cd007c419bc4772e263fdf71894c5
REYGON2468 <gonzalo.longatti@davinci.edu.ar>
2024-02-08 19:14:17 -0300

    StoreWoodNewFix

--- Assets/Scripts/Extras/Store.cs
+++ Assets/Scripts/Extras/Store.cs
@@ -150,102 +150,102 @@ public class Store : MonoBehaviour
 
         #region woodQuantity_If_False
 
-        if (woodQuantity >= 100)
+        if (woodQuantity < 100)
         {
             _woods[0].SetActive(false);
         }
 
-        if (woodQuantity >= 200)
+        if (woodQuantity < 200)
         {
             _woods[1].SetActive(false);
         }
 
-        if (woodQuantity >= 300)
+        if (woodQuantity < 300)
         {
             _woods[2].SetActive(false);
         }
 
-        if (woodQuantity >= 400)
+        if (woodQuantity < 400)
         {
             _woods[3].SetActive(false);
         }
 
-        if (woodQuantity >= 500)
+        if (woodQuantity < 500)
         {
             _woods[4].SetActive(false);
         }
 
-        if (woodQuantity >= 600)
+        if (woodQuantity < 600)
         {
             _woods[5].SetActive(false);
         }
 
-        if (woodQuantity >= 700)
+        if (woodQuantity < 700)
         {
             _woods[6].SetActive(false);
         }
 
-        if (woodQuantity >= 800)
+        if (woodQuantity < 800)
         {
             _woods[7].SetActive(false);
         }
 
-        if (woodQuantity >= 900)
+        if (woodQuantity < 900)
         {
             _woods[8].SetActive(false);
         }
 
-        if (woodQuantity >= 1000)
+        if (woodQuantity < 1000)
         {
             _woods[9].SetActive(false);
         }
 
-        if (woodQuantity >= 1100)
+        if (woodQuantity < 1100)
         {
             _woods[10].SetActive(false);
         }
 
-        if (woodQuantity >= 1200)
+        if (woodQuantity < 1200)
         {
             _woods[11].SetActive(false);
         }
 
-        if (woodQuantity >= 1300)
+        if (woodQuantity < 1300)
         {
             _woods[12].SetActive(false);
         }
 
-        if (woodQuantity >= 1400)
+        if (woodQuantity < 1400)
         {
             _woods[13].SetActive(false);
         }
 
-        if (woodQuantity >= 1500)
+        if (woodQuantity < 1500)
         {
             _woods[14].SetActive(false);
         }
 
-        if (woodQuantity >= 1600)
+        if (woodQuantity < 1600)
         {
             _woods[15].SetActive(false);
         }
 
-        if (woodQuantity >= 1700)
+        if (woodQuantity < 1700)
         {
             _woods[16].SetActive(false);
         }
 
-        if (woodQuantity >= 1800)
+        if (woodQuantity < 1800)
         {
             _woods[17].SetActive(false);
         }
 
-        if (woodQuantity >= 1900)
+        if (woodQuantity < 1900)
         {
             _woods[18].SetActive(false);
         }
 
-        if (woodQuantity >= 2000)
+        if (woodQuantity < 2000)
         {
             _woods[19].SetActive(false);
         }
